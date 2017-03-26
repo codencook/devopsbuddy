8f052237371c1428ff4f6333a7bbada8116e951c
codencook <arajman1@binghamton.edu>
2017-03-26 18:29:21 -0400

    added about page

---
+++ src/main/java/com/devopsbuddy/web/controllers/CopyController.java
@@ -0,0 +1,18 @@
+package com.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Aniket on 3/26/2017.
+ */
+
+@Controller
+public class CopyController
+{
+    @RequestMapping("/about")
+    public String about()
+    {
+        return "copy/about";
+    }
+}
